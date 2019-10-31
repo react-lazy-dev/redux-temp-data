ed485b3d16aaabe67e76c6f464810bb95f458917
Amir Beshkar <amir.beshkar@gmail.com>
2019-11-01 01:51:07 +0330

    implement the selector

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,5 @@ export type TempDataState = Record<string, TempDataRecord>;
 export interface TempDataRootState {
   tempData: TempDataState;
 }
+
+export type SelectorFunction<T, U> = (data: T) => U;
