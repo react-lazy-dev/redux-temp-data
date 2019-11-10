958fd01f97cbb3f468059f36d86707656d8dbe61
Amir Beshkar <amir.beshkar@gmail.com>
2019-11-10 22:28:23 +0330

    add @testing-library/react and @testing-library/jest-dom modules to the project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as useTempData } from "./useTempData";
-export { default as withTempData } from "./withTempData";
+// export { default as useTempData } from "./useTempData";
+// export { default as withTempData } from "./withTempData";
 export { default as tempDataReducer } from "./reducer";
 export { default as tempDataSelector } from "./selector";
 export { default as setupTempDataCleanup } from "./setupCleanup";
